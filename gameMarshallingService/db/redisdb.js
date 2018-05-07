c323413960769f8d163d50d2220ac7ed7dbaef1b
willyspinner <wilsonjusuf1998@gmail.com>
2018-05-07 02:14:15 -0700

    a crap ton of redis testing done

--- gameMarshallingService/db/redisdb.js
+++ gameMarshallingService/db/redisdb.js
@@ -6,12 +6,26 @@ const redisconnectionobject = {
 }
 const promisify = require('util').promisify;
 let redisclient = redis.createClient(redisconnectionobject);
-// promisifying our redis commands:
+//  <<<<< promisifying our redis commands >>>>>:
+// lists commands:
 const redisRpushAsync = promisify(redisclient.rpush).bind(redisclient);
-const redisRpopAsync = prommisify(redisclient.rpop).bind(redisclient);
-const redisSetAsync = promisify(redisclient.set).bind(redisclient);
+const redisRpopAsync = promisify(redisclient.rpop).bind(redisclient);
+const redisLpushAsync = promisify(redisclient.lpush).bind(redisclient); // for popping pile to hand.
 const redisLrangeAsync = promisify(redisclient.lrange).bind(redisclient);
-
+const redisLlenAsync = promisify(redisclient.llen).bind(redisclient);
+const redisLindexAsync = promisify(redisclient.lindex).bind(redisclient);
+// get and set commands:
+const redisSetAsync = promisify(redisclient.set).bind(redisclient);
+const redisGetAsync = promisify(redisclient.get).bind(redisclient);
+const redisIncrAsync = promisify(redisclient.incr).bind(redisclient);
+// sorted set commands
+const redisZaddAsync = promisify(redisclient.zadd).bind(redisclient);
+const redisZcountAsync = promisify(redisclient.zcount).bind(redisclient);
+const redisZrangeByScoreAsync = promisify(redisclient.zrangebyscore).bind(redisclient);
+// scan command
+const redisScanAsync = promisify(redisclient.scan).bind(redisclient);
+// del command:
+const redisDelAsync = promisify(redisclient.del).bind(redisclient);
 
 // NOTE don't mix application logic. Keep this middleware dumb. Validation should happen
 // on the main gmsapp.js
@@ -22,36 +36,45 @@ All the redis db does is handle the cards for us. nothing else
 {{{{   data structures   }}}}
 for every game session GAMESESSIONID (note this is different from the lobby id),
 we have:
--  GAMESESSIONID/cardpile - redis list for the pile.
+-  GAMESESSIONID/pile - redis list for the pile.
 -  GAMESESSIONID/nplayers - int of number of players.
 -  GAMESESSIONID/counter - int of current counter (not % 13 though .this is absolute) - mod by 13 will be done by server.
--  GAMESESSIONID/slappedusers - redis hashtable for users who slapped.
--  GAMESESSIONID/turnindex - the index of who is in turn to throw, based on 'users' array (below) (0 to nplayers - 1).
--  GAMESESSIONID/users - redis list of the players (ordered).
+-  GAMESESSIONID/slappedusers - redis sorted set for users who slapped.
+- GAMESESSIONID/playerinturn - the player who is supposed to throw.
+-  GAMESESSIONID/players - redis set of the players (ordered).
 
-for every user USERNAME in GAMESESSIONID, we have:
--  GAMESESSIONID/USERNAME/hand - redis list of the user's hand.
+for every player with username USERNAME in GAMESESSIONID, we have:
+-  GAMESESSIONID/player/USERNAME/hand - redis list of the user's hand.
+- GAMESESSIONID/player/USERNAME/streaks - int of how many times (consecutively) a user has no pile.
 
 
 {{{{    methods:    }}}}
 State mutations:
+-# initialise game
+-# delete game
 -# pop hand and push card to pile
         - returns popped card.
-
 -# pop card pile and transfer to loser's hand(identified by loser's username)
--# increment current counter (and get counter result then)
+-# increment current counter (and get next player's turn).
 -# register a user's slap (push username to slap hashtable, where key: value = username: slaptime(ms)) - remember that slaptime is determined client-side.
     - simultaneously returns how many users slapped
+-# increment a person's streak.
+    - return streak
+-# set person's streak to 0
 
 gets:
 -# get current turn
 -# get game card pile (in order) ( for testing purposes only)
 -# get snapshot of game (everyone's piles)
 -# get number of players.
--# get slappedUsers hashtable. (for our gms app)
+-# get slappedUsers Sorted set. (for our gms app)
  */
 
 const self = module.exports = {
+
+
+    //      {{{{    State mutation methods    }}}}
+
     /* initialize new game with:
          players -  (array of usernames)
      returns:
@@ -72,11 +95,11 @@ const self = module.exports = {
                     console.log(`returning promise for ${player}`);
                     return new Promise((resolve, reject) => {
                         Promise.all([
-                            redisRpushAsync(`${gamesessionid}/users`, player),
+                            redisRpushAsync(`${gamesessionid}/players`, player),
                             Promise.all(
                                 Array.from(Array(cardsperplayer)).map((c) => {
                                     console.log(`adding card to ${gamesessionid}/${player}`);
-                                    return redisRpushAsync(`${gamesessionid}/${player}/hand`, `${cards.fullcarddeck[Math.floor(Math.random() * cards.fullcarddeck.length)]}`);
+                                    return redisRpushAsync(`${gamesessionid}/player/${player}/hand`, `${cards.fullcarddeck[Math.floor(Math.random() * cards.fullcarddeck.length)]}`);
                                 })
                             ),
                         ]).then((data) => {
@@ -85,27 +108,24 @@ const self = module.exports = {
                         }).catch((e) => reject(e));
                     });
                 }).concat(
-                [
-                    redisSetAsync(`${gamesessionid}/nplayers`, `${players.length}`),
-                    redisSetAsync(`${gamesessionid}/counter`, 0),
-                    redisSetAsync(`${gamesessionid}/turnindex`, `${Math.floor(Math.random() * players.length)}`)
-                ]
+                    [
+                        redisSetAsync(`${gamesessionid}/nplayers`, `${players.length}`),
+                        redisSetAsync(`${gamesessionid}/counter`, 0),
+                    ]
                 )).then(() => {
-                    Promise.all(
-                        players.map((player) => {
-                            return redisLrangeAsync(`${gamesessionid}/${player}/hand`, 0, cardsperplayer - 1).then((data) => {
-                                console.log(`redis lrangeasync ok for ${gamesessionid}/${player}/hand. ${JSON.stringify(data)} `);
-                                snapshotobj[player] = data;
-                                console.log(`snapshot object : ${JSON.stringify(snapshotobj)}`);
-                            });
-                        })
-
-                    ).then((data) => {
-                        resolve(snapshotobj);
-                    }).catch((e) => {
-                        console.log(`REJECT 1`);
-                        reject(e)
-                    });
+                Promise.all([
+                    ...players.map((player) => {
+                        return redisLrangeAsync(`${gamesessionid}/player/${player}/hand`, 0, cardsperplayer - 1).then((data) => {
+                            snapshotobj[player] = data;
+                        });
+                    }),
+                    redisSetAsync(`${gamesessionid}/playerinturn`,players[0]) // first player to play is [0].
+                    ]).then((data) => {
+                    resolve(snapshotobj);
+                }).catch((e) => {
+                    console.log(`REJECT 1`);
+                    reject(e)
+                });
             }).catch((e) => {
                     console.log(`REJECT 2`);
                     reject(e)
@@ -114,18 +134,190 @@ const self = module.exports = {
         })
     },
 
-    // pops player's card and pushes to the pile.
-    // returns player's card popped.
-    popHandToPile: (gamesessionid,player)=>{
-       return new Promise((resolve,reject)=>{
-           redisRpopAsync(`${gamesessionid}/${player}/hand`).then((poppedCard)=>{
-               redisRpushAsync(`${gamesessionid}/pile`,poppedCard).then(()=>{
-                   resolve(poppedCard);
-               }).catch((e)=>reject(e));
+    deleteGame: (gamesessionid)=>{
+         return new Promise((resolve,reject)=>{
+             //note: returnArr is here because this is used for the scan.
+             // scan is a recursive function.
+            scanAsync(0,`${gamesessionid}/*`, (keys)=>{
+                redisDelAsync(...keys).then((retcode)=>{
+                    if(keys.length === retcode)
+                        resolve(retcode);
+                    else{
+                        console.error(`redisdb::deleteGame: when trying to delete
+                         ${gamesessionid},deleted ${retcode} instead of ${keys.length} 
+                         found by scan`);
+                        resolve(retcode);
+                    }
+                }).catch(e=>reject(e));
+            });
+        });
+    },
+    popHandToPile: (gamesessionid, player) => {
+        return new Promise((resolve, reject) => {
+            redisRpopAsync(`${gamesessionid}/player/${player}/hand`).then((poppedCard) => {
+                redisRpushAsync(`${gamesessionid}/pile`, poppedCard).then(() => {
+                    resolve(poppedCard);
+                }).catch((e) => reject(e));
+            }).catch((e) => reject(e));
+        });
+    },
+
+    popPileToLoser: (gamesessionid, loser) => {
+        return new Promise((resolve, reject) => {
+            // 1. First, pop the pile.
+            redisLlenAsync(`${gamesessionid}/pile`).then((len) => {
+                Promise.all(
+                    Array.from(Array(len)).map(m =>
+                        redisRpopAsync(`${gamesessionid}/pile`)
+                    )
+                ).then((poppedpile) => {
+                    console.log(`poppedpile: ${JSON.stringify(poppedpile)}`);
+                    // 2. , push the popped pile cards to loser.
+                    redisLpushAsync(`${gamesessionid}/player/${loser}/hand`, ...poppedpile)
+                        .then((result) => {
+                            resolve();
+                        }).catch(e => reject(e));
+                }).catch(e => reject(e));
+            }).catch(e => reject(e));
+        }).catch(e => reject(e));
+    },
+
+    //NOTE: only increments the counter AND player in turn.
+    // doesn't pop or anything. Forthis, pophandtopile must be called.
+    // this is because we don't want to couple pop hand and increment.
+    // what if e.g. the user pops hand on a match event (Which is an illegal op)?
+    incrementCurrentCounter: (gamesessionid) => {
+        return new Promise((resolve, reject) => {
+            redisIncrAsync(`${gamesessionid}/counter`).then((newcounter) => {
+                redisGetAsync(`${gamesessionid}/nplayers`).then((nplayers) => {
+                    redisLindexAsync(`${gamesessionid}/players`, newcounter % nplayers).then((nextplayer) => {
+                        redisSetAsync(`${gamesessionid}/playerinturn`, nextplayer).then((retcode) => {
+                            resolve(nextplayer);
+                        }).catch(e => reject(e));
+                    }).catch(e => reject(e))
+                }).catch(e => reject(e));
+            }).catch(e => reject(e));
+        });
+    },
+
+    slap: (gamesessionid, player, reactiontime) => {
+        return new Promise((resolve, reject) => {
+            redisZaddAsync(`${gamesessionid}/slappedusers`, reactiontime, player).then(() => {
+                redisZrangeByScoreAsync(`${gamesessionid}/slappedusers`, '-inf', '+inf').then((slappedplayers) => {
+                    resolve(slappedplayers);
+                }).catch((e) => reject(e));
+            }).catch((e) => reject(e));
+        });
+    },
+
+    incrementStreak: (gamesessionid, player) => {
+        return new Promise((resolve, reject) => {
+            redisIncrAsync(`${gamesessionid}/player/${player}/streak`).then((newstreak) => {
+                resolve(newstreak);
+            }).catch(e => reject(e));
+        });
+    },
+
+    setZeroSreak: (gamesessionid, player) => {
+        return new Promise((resolve, reject) => {
+            redisSetAsync(`${gamesessionid}/player/${player}/streak`).then((newstreak) => {
+                resolve(0);
+            }).catch(e => reject(e));
+        });
+    },
+    //      {{{{    Get methods    }}}}
+
+    // get current turn. NOTE: idt we need this.
+    getCurrentTurn: (gamesessionid) => {
+        return new Promise((resolve, reject) => {
+            redisGetAsync(`${gamesessionid}/playerinturn`).then(playerinturn => {
+                resolve(playerinturn);
+            }).catch((e) => reject(e));
+        });
+    },
+
+    // get game card pile.
+    getPile: (gamesessionid) => {
+        return new Promise((resolve, reject) => {
+            redisLlenAsync(`${gamesessionid}/pile`).then(len => {
+                redisLrangeAsync(`${gamesessionid}/pile`).then(pile => {
+                    resolve(pile);
+                }).catch(e => reject(e));
+            }).catch(e => reject(e));
+        }).catch(e => reject(e));
+    },
+
+    // get snapshot of the game, i.e. everyone's cards.
+    getSnapshot: (gamesessionid) => {
+        return new Promise((resolve, reject) => {
+            let returnArr = [];
+            scanAsync("0", `${gamesessionid}/player/*/hand`, (handkeys) => {
+                console.log(`handkeys got: ${JSON.stringify(handkeys)}`);
+                Promise.all(
+                    handkeys.map(handkey =>
+                        new Promise((res1, rej1) => {
+                            redisLlenAsync(handkey).then(len => {
+                                redisLrangeAsync(handkey, 0, len - 1).then((hand) => {
+                                    const username = handkey.split('/')[2];
+                                    res1({username: username, hand: hand});
+                                })
+                            }).catch(e => rej1(e));
+                        })
+                    )
+                ).then((hands) => {
+                    // hands is an array containing elements like:
+                    // {username:username, hand: [c1,c2,c3,...]}
+                    resolve(hands);
+                }).catch(e => reject(e))
+            }).catch(e => {
+                console.log(`redisdb::getSnapshot : scanAsync failed!`);
+                reject(e);
+            });
+        });
+    },
+
+    // get number of players
+    getNplayers: (gamesessionid)=>{
+        return new Promise((res,rej)=>{
+            redisGetAsync(`${gameasessionid}/nplayers`).then((len)=>{
+                res(len);
+            }).catch((e)=>rej(e));
+        });
+    },
+
+    // get slapped users, with their reaction times.
+   getSlappedPlayers: (gamesessionid)=>{
+       return new Promise((resolve,reject)=> {
+               redisZrangeByScoreAsync(`${gamesessionid}/slappedusers`,0,"+inf",'WITHSCORES').then((slappedusers)=>{
+                   let retObj = [];
+                   for(let i = 0; i +1 < slappedusers.length ; i+=2){
+                       retObj.push({username: slappedusers[i], reactiontime: slappedusers[i+1]});
+                   }
+                   resolve(retObj);
            }).catch((e)=>reject(e));
        });
+       },
+    utils: {
+        scanAsync: scanAsync,
     }
+   }
 
-
-}
 //helper stuff
+// returns promises for scan just incase non 0
+function scanAsync(cursor, pattern,callback,returnArr=[]) {
+    return redisScanAsync(cursor, "MATCH", pattern, "COUNT", "40").then(
+        function (reply) {
+            cursor = reply[0];
+            var keys = reply[1];
+            keys.forEach(function (key, i) {
+                returnArr.push(key);
+            });
+            if (cursor === '0') {
+                return callback(returnArr);
+            } else {
+                return scanAsync(cursor, pattern, callback, returnArr)
+            }
+
+        });
+};
+
--- gameMarshallingService/tests/redisdb.test.js
+++ gameMarshallingService/tests/redisdb.test.js
@@ -2,20 +2,209 @@ require('dotenv').config({path: `${__dirname}/../.gms.test.env`});
 const dummydata = require('./dummydata/dummydata');
 const assert = require('assert');
 const redisdb = require('../db/redisdb');
-describe('Test init game',function(){
+
+describe('redisdb.test: init and delete game',function(){
     it('should initialise our dummy game',function(done){
         redisdb.initializeGame(dummydata.game1.gamesessionid,
             dummydata.game1.players,dummydata.game1.cardsperplayer)
             .then((snapshot)=>{ // remember that snapshot are kv pairs of user: [c1,c2,,..]
                 // where c1 c2 are the cards.
+                
+                console.log(`mocha: got snapshot: ${JSON.stringify(snapshot)}`);
                    Object.keys(snapshot).forEach((player)=>{
                        const playerhand = snapshot[player];
                        assert.equal(playerhand.length,dummydata.game1.cardsperplayer);
-                       
                    });
                 done();
             }).catch((e)=>{
                 done(new Error('failed'));
             })
         });
+    it('should delete our dummy game, verified by an empty SCAN',function(done){
+
+        redisdb.deleteGame(dummydata.game1.gamesessionid).then((retcode)=>{
+            // confirm that in redis scan, there is none.
+            redisdb.utils.scanAsync(0,`${dummydata.game1.gamesessionid}/*`,(keys)=>{
+                assert.equal(keys.length,0);
+                
+                console.log(`delete dummy game ok .`);
+                done();
+            })
+        }).catch((e)=>done(e));
+    });
+});
+
+describe('redisdb.test: test popHandToPile', function(){
+    before(function(done){
+        redisdb.initializeGame(dummydata.game1.gamesessionid,
+            dummydata.game1.players,dummydata.game1.cardsperplayer)
+            .then((snapshot)=>{ // remember that snapshot are kv pairs of user: [c1,c2,,..]
+                // where c1 c2 are the cards.
+                Object.keys(snapshot).forEach((player)=>{
+                    const playerhand = snapshot[player];
+                    assert.equal(playerhand.length,dummydata.game1.cardsperplayer);
+                });
+                done();
+            }).catch((e)=>{
+            done(new Error('failed'));
+        })
+    });
+    it('should pop hand to pile correctly',function(done){
+        redisdb.getSnapshot(dummydata.game1.gamesessionid).then((snapshot)=>{
+            redisdb.popHandToPile(dummydata.game1.gamesessionid,
+                dummydata.game1.players[0]
+            ).then((poppedcard)=>{
+                    const hand= snapshot.find((elem)=>
+                        elem.username === dummydata.game1.players[0]
+                    ).hand;
+                    const expectedcard = hand[hand.length - 1];
+                    assert.equal(expectedcard,poppedcard);
+                    done();
+            }).catch(e=>done(e));
+        })
     })
+});
+
+describe('redisdb.test: increment current counter testing',function(){
+    before(function(done) {
+        redisdb.initializeGame(dummydata.game1.gamesessionid,
+            dummydata.game1.players, dummydata.game1.cardsperplayer)
+            .then((snapshot) => {
+                done()
+            }).catch((e) => done(e));
+    });
+    after(function(done){
+        redisdb.deleteGame(dummydata.game1.gamesessionid).then(()=>{
+            done();
+        }).catch(e=>done(e));
+    });
+    it('should be in order before moduloing.',function(done) {
+        redisdb.getCurrentTurn(dummydata.game1.gamesessionid).then((playerinturn) => {
+            assert.equal(playerinturn, dummydata.game1.players[0]);
+            redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer) => {
+                assert.equal(nextplayer, dummydata.game1.players[1]);
+                redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer2) => {
+                    assert.equal(nextplayer2, dummydata.game1.players[2]);
+                    redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer3) => {
+                        assert.equal(nextplayer3, dummydata.game1.players[3]);
+                        redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer4) => {
+                            assert.equal(nextplayer4, dummydata.game1.players[4]);
+                            done();
+                        }).catch((e) => done(e));
+                    }).catch((e) => done(e));
+                }).catch((e) => done(e));
+            }).catch((e) => done(e));
+        }).catch((e) => done(e));
+    });
+    it('should be in order modulo nplayers',function(done){
+        redisdb.getCurrentTurn(dummydata.game1.gamesessionid).then((playerinturn) => {
+            assert.equal(playerinturn, dummydata.game1.players[4]);
+            redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer) => {
+                assert.equal(nextplayer, dummydata.game1.players[0]);
+                redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer2) => {
+                    assert.equal(nextplayer2, dummydata.game1.players[1]);
+                    redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer3) => {
+                        assert.equal(nextplayer3, dummydata.game1.players[2]);
+                        redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer4) => {
+                            assert.equal(nextplayer4, dummydata.game1.players[3]);
+                            redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer5) => {
+                                assert.equal(nextplayer5, dummydata.game1.players[4]);
+                                redisdb.incrementCurrentCounter(dummydata.game1.gamesessionid).then((nextplayer6) => {
+                                    assert.equal(nextplayer6, dummydata.game1.players[0]);
+                                done();
+                                }).catch((e) => done(e));
+                            }).catch((e) => done(e));
+                        }).catch((e) => done(e));
+                    }).catch((e) => done(e));
+                }).catch((e) => done(e));
+            }).catch((e) => done(e));
+        }).catch((e) => done(e));
+    });
+});
+
+describe('redisdb.test: slaps',function(){
+    beforeEach(function(done) {
+        redisdb.initializeGame(dummydata.game1.gamesessionid,
+            dummydata.game1.players, dummydata.game1.cardsperplayer)
+            .then((snapshot) => {
+                done()
+            }).catch((e) => done(e));
+    });
+    afterEach(function(done){
+        redisdb.deleteGame(dummydata.game1.gamesessionid).then(()=>{
+            done();
+        }).catch(e=>done(e));
+    });
+    it('should register the slap on return obj, and on redis', function (done) {
+        redisdb.slap(dummydata.game1.gamesessionid,
+            dummydata.game1.players[3],1.62).then((slappedplayers)=>{
+                assert.equal(slappedplayers.length,1); // only one slapped.
+                assert.equal(slappedplayers[0],dummydata.game1.players[3]);
+                done();
+        }).catch(e=>done(e));
+    });
+    it('should register multiple slaps and rank increasing reaction time' +
+        ' (fastest first, slowest last)', function (done) {
+        const sessid = dummydata.game1.gamesessionid;
+        const players = dummydata.game1.players;
+        // we just simulate (although unecessarily) the conditions for the slap.
+        // some slap first, others slap later. Then we see what happens in the end.
+        Promise.all([
+            new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    redisdb.slap(sessid,players[0],JSON.stringify(0.300)).then(()=>{
+                        resolve();
+                    }).catch(e=>reject(e));
+                },300)
+           }),
+            new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    
+
+                    redisdb.slap(sessid,players[1],JSON.stringify(0.200)).then(()=>{
+                        resolve();
+                    }).catch(e=>reject(e));
+                },200)
+            }),
+            new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    redisdb.slap(sessid,players[2],JSON.stringify(0.750)).then(()=>{
+                        resolve();
+                    }).catch(e=>reject(e));
+                },750)
+            }),
+            new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    redisdb.slap(sessid,players[3],JSON.stringify(0.749)).then(()=>{
+                        resolve();
+                    }).catch(e=>reject(e));
+                },749)
+            }),
+            new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    redisdb.slap(sessid,players[4],JSON.stringify(0.650)).then(()=>{
+                        resolve();
+                    }).catch(e=>reject(e));
+                },650)
+            })
+        ]).then((slapObjects)=>{
+            assert.equal(slapObjects.length,5);
+            redisdb.getSlappedPlayers(sessid).then((slappedplayers)=>{
+                // from slowest to fastest.
+                
+                console.log(`slapped playersobj : ${JSON.stringify(slappedplayers)}`);
+                assert.equal(slappedplayers.pop().username,players[2]);
+                assert.equal(slappedplayers.pop().username,players[3]);
+                assert.equal(slappedplayers.pop().username,players[4]);
+                assert.equal(slappedplayers.pop().username,players[0]);
+                assert.equal(slappedplayers.pop().username,players[1]);
+                done();
+            }).catch(e=>done(e));
+        }).catch(e=>done(e));
+    });
+});
+
+describe('redisdb.test: incrementing streaks', function () {
+
+});
+
