f6536343f56b6365953b6baaafc3f5e867e356b3
willyspinner <wilsonjusuf1998@gmail.com>
2018-06-27 19:37:31 +0700

    chat has scrolling functionality (automatic) but wont scroll till lowest. Need to fix this.

--- frontend/src/components/ui/ChatRoom.js
+++ frontend/src/components/ui/ChatRoom.js
@@ -21,9 +21,13 @@ This is updated through props.
  */
 class ChatRoom extends Component {
 
+    constructor(props){
+        super(props);
+    }
+
     state = {inputval : ''};
-    onPressEnterHandler = (e) =>{
-        if (e.target.value !== ""){
+    onPressEnterHandler = (e) => {
+        if (e.target.value !== "") {
             // SEND STUFF HERE
             console.log('pressed enter for chat.');
             this.props.onMessageSend(e.target.value);
@@ -33,42 +37,37 @@ class ChatRoom extends Component {
     renderItem = (index, key) =>{
         const item = this.props.messageFeed[index];
         return (
-        <List.Item key={key}>
-            <List.Item.Meta
-                avatar={ <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />}
-                title={
-                    <div>
-                        <h4>{item.sender_username}</h4>
-                        <h5>
-                            {moment(item.timestamp).format(DATE_FORMAT)}
-                        </h5>
-                    </div>
-                }
-                description={item.message}
-            />
-            <br/>
+            <div key={key}>
+                <h3>
+                    {item.sender_username}: {item.message}
+                </h3>
+                <h5 style ={{color:'grey'}}>
+                    {moment(item.timestamp).format(DATE_FORMAT)}
+                </h5>
+            </div> )
 
-        </List.Item>)
     }
 
     render() {
+        if(this.reffy)
+            this.reffy.scrollTo(this.reffy.getVisibleRange()[1] );
         return (
-            <div>
-                <h1>Chat</h1>
-                <div style={{overflow: 'auto', maxHeight: 400}}>
+            <div style={{height : 350}}>
+                <div style={{overflow: 'auto', maxHeight: 300}}>
                     <ReactList
                         itemRenderer={this.renderItem}
                         length={this.props.messageFeed.length}
                         type='uniform'
+                        ref={ref=>{this.reffy = ref;}}
                     />
-                    <Input
-                        onChange={(e)=>this.setState({inputval:e.target.value})}
-                        value={this.state.inputval}
-                        placeholder={"type your message here"}
-                        onPressEnter={this.onPressEnterHandler}
-                    >
-                    </Input>
                 </div>
+                <Input
+                    onChange={(e)=>this.setState({inputval:e.target.value})}
+                    value={this.state.inputval}
+                    placeholder={"type your message here"}
+                    onPressEnter={this.onPressEnterHandler}
+                >
+                </Input>
             </div>
         );
     }
