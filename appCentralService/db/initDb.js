4be25accd83643b969f5ef43b96c914dd7b39113
willyspinner <wilsonjusuf1998@gmail.com>
2018-07-08 22:34:10 +0700

    renamed appcs's app.js to appcsapp.ks

--- appCentralService/db/initDb.js
+++ appCentralService/db/initDb.js
@@ -4,7 +4,9 @@ const db = require('./db');
 const logger = require('../log/appcs_logger');
 db.initTables().then(()=>{
         logger.info("INIT DB", "DB Initialized");
+        process.exit(0);
 }).catch(()=>{
     logger.error("INIT DB","DB could not be initialized.")
+    process.exit(1);
 })
 
