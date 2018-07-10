75148e200a9ae390b50522d9c65b38987eb0461b
willyspinner <wilsonjusuf1998@gmail.com>
2018-07-11 03:43:02 +0700

    started work on ui intools.

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -4,6 +4,7 @@ import {Router,Switch,Route } from 'react-router-dom';
 import MainPage from '../components/MainPage';
 import GameLobbyPage from '../components/GameLobbyPage';
 import GamePlayPage from '../components/GamePlayPage';
+import IntoolsUIRouter from './IntoolsUIRouter';
 import Footer from '../components/Footer';
 export const history = createHistory();
 const AppRouter =()=> (
@@ -13,7 +14,12 @@ const AppRouter =()=> (
                 <Route path="/" component={MainPage} exact = {true}/>
                 <Route path="/game/lobby/:uuid" component={GameLobbyPage} exact = {true}/>
                 <Route path="/game/play/:uuid" component={GamePlayPage} exact = {true}/>
-                <Route component={()=>(<h1> ROUTE NOT FOUND</h1>)}/>
+                { /* NOTE: only show intools in development.*/
+                   process.env.NODE_ENV !== 'production'?
+                    (<Route path="/IntoolsUI" component={IntoolsUIRouter} exact={false}/>)
+                    : null
+                }
+                <Route render={(<h1> ROUTE NOT FOUND</h1>)}/>
             </Switch>
             <Footer className="appFooter"/>
         </div>
