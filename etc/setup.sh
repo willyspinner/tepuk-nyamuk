9bfa824ba9babdf892f038da7685a6f4962b38d4
willyspinner <wilsonjusuf1998@gmail.com>
2018-07-12 23:05:28 +0700

    improved bash script

--- etc/setup.sh
+++ etc/setup.sh
@@ -3,18 +3,15 @@
 # NOTE: This setup script assumes sudo privileges, an ubuntu 16.04 environment and gcc.
 
 source functions.sh;
-
 if [ $EUID -ne 0 ];
 then
 error_exit 'You need to run this script as root.'
 exit 1
 fi
-
 if [ "$(uname -s)" == 'Darwin' ];
 then
 error_exit 'You cannot run this in MAC OSX. Need Ubuntu 16.04'
 fi
-
 if [ -e setup.sh ]; then echo ' running setup .sh ...'; else
     error_exit "Please run setup.sh in the same directory.";
 fi
@@ -24,9 +21,9 @@ pushd .
 sudo apt-get update
 yes |sudo apt-get upgrade
 
+
 # install vim goodies
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
 echo "$vimrccontents" > ~/.vimrc;
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
@@ -41,18 +38,26 @@ mkdir ~/.tepuknyamuk
 mkdir ~/willysServerBin
 mkdir ~/projects
 mkdir ~/applications
-
 cp tools/redis-daemon.conf ~/.tepuknyamuk
 cp tools/colors ~/willysServerBin
 cp tools/.aliases ~
 cp tools/.startup ~
-#//cp ../scripts/* ~/willysServerBin/
+pushd .
+cd ~/.tepuknyamuk;
+
+
+# replace Pid file for redis daemonize.
+sed -i -e "s@REPLACE_PIDFILE@$(pwd)/redis-server-daemon.pid@g" ~/.tepuknyamuk/redis-daemon.conf
+popd;
+cd ../scripts;
+
+
+# put tepuk nyamuk scripts as path.
 scriptsdir="$(pwd)"
 
 
-# put willysServerBin as path.
+# put willysServerBin as path and configure .bashrc.
 cd ~/willysServerBin;
-
 echo "export PATH=\"\$PATH:$PWD" >> ~/.bashrc
 echo "export PATH=\"\$PATH:$scriptsdir" >> ~/.bashrc
 echo "source ~/.aliases" >> ~/.bashrc
@@ -60,17 +65,16 @@ echo "source ~/.startup" >> ~/.bashrc
 
 # install redis
 cd ~/applications
-
 curl -O http://download.redis.io/redis-stable.tar.gz || error_exit "couldn't download redis. Check internet connection?"
 tar xzvf redis-stable.tar.gz && rm redis-stable.tar.gz
 cd redis-stable
 make && sudo make install && ln -s src/redis-server ~/willysServerBin/redis-server && ln -s src/redis-cli ~/willysServerBin/redis-cli
 
 
-
 # install apt packages (htop, postgres, ncdu, nginx, toilet, figlet)
 yes | sudo apt-get install htop postgresql postgresql-contrib ncdu nginx figlet toilet || error_exit "couldn't download apt packages.";
 sudo systemctl start
+
 
 # install node js
 command -v node || ( curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && yes| sudo apt-get install -y nodejs)
